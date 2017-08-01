036a74dd0287602624e7be54ac675476a855b126
park kyung suk <boardguy1024@gmail.com>
2017-08-01 16:38:28 +0900

    Initial Commit

---
+++ SideBarWithAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SideBarWithAnimation
+//
+//  Created by park kyung suk on 2017/08/01.
+//  Copyright © 2017年 park kyung suk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
