2c7897011cfa7292e4a9b1f6d11c6bede3e38327
park kyung suk <boardguy1024@gmail.com>
2017-08-02 00:36:02 +0900

    サイドメニューがカーブアニメーションに表示されるように修正

--- SideBarWithAnimation/ViewController.swift
+++ SideBarWithAnimation/ViewController.swift
@@ -13,9 +13,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var sideBarView: UIView!
     @IBOutlet weak var menuButton: UIButton!
     @IBOutlet weak var screenCoverButton: UIButton!
+    @IBOutlet weak var menuCurveImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        menuCurveImageView.image = #imageLiteral(resourceName: "side")
+        menuCurveImageView.tintColor = .yellow
         sideBarView.alpha = 0
         screenCoverButton.alpha = 0
     }
@@ -25,24 +28,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @IBAction func menuButtonTappd(_ sender: Any) {
-        hideMenu()
+        showMenu()
         
     }
     @IBAction func screenCoverButtonTapped(_ sender: Any) {
-        showMenu()
+        hideMenu()
     }
     
     func showMenu() {
-        UIView.animate(withDuration: 0.5) {
-            self.screenCoverButton.alpha = 0
-            self.sideBarView.alpha = 0
-        }
-    }
-    func hideMenu() {
-        UIView.animate(withDuration: 0.5) {
+        UIView.animate(withDuration: 0.4) {
             self.screenCoverButton.alpha = 0.8
             self.sideBarView.alpha = 1
         }
+        UIView.animate(withDuration: 0.4, delay: 0, options: .curveEaseIn, animations: {
+            self.menuCurveImageView.transform = CGAffineTransform.identity
+        })
+        
+    }
+    func hideMenu() {
+        UIView.animate(withDuration: 0.4) {
+            self.screenCoverButton.alpha = 0
+            self.sideBarView.alpha = 0
+        }
+        UIView.animate(withDuration: 0.4, delay: 0, options: .curveEaseOut, animations: { 
+         
+            //隠す時はwidth分マイナス方向に配置しておく
+            self.menuCurveImageView.transform = CGAffineTransform(translationX: -self.menuCurveImageView.frame.width, y: 0)
+            
+        })
     }
     
     
